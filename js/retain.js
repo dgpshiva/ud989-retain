1bee568273965b277b91eb36f1871e2a68788b18
Shiva <SRamasubramanian@slb.com>
2018-06-05 16:13:15 -0500

    Adding a new date proprty to model

--- js/retain.js
+++ js/retain.js
@@ -20,7 +20,8 @@ $(function(){
     var octopus = {
         addNewNote: function(noteStr) {
             model.add({
-                content: noteStr
+                content: noteStr,
+                dateSubmitted: Date.now()
             });
             view.render();
         },
@@ -51,9 +52,10 @@ $(function(){
         render: function(){
             var htmlStr = '';
             octopus.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
-                        note.content +
-                    '</li>';
+                htmlStr += '<li class="note">'
+                            + note.content
+                            + '<span class="note-date">' + new Date(note.dateSubmitted).toString() + '</span>'
+                            + '</li>'
             });
             this.noteList.html( htmlStr );
         }
